704c5d463125b31b0e12582c089cc358a111d90b
Doruk Karakoç <karakocdoruk4@gmail.com>
2025-01-23 23:36:37 +0300

    gradle update

--- src/main/java/com/team3390/robot/subsystems/Elevator.java
+++ src/main/java/com/team3390/robot/subsystems/Elevator.java
@@ -5,7 +5,7 @@
 package com.team3390.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.team3390.lib.drivers.LazyTalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.team3390.lib.drivers.TalonSRXCreator;
 import com.team3390.lib.drivers.TalonSRXCreator.Configuration;
 import com.team3390.robot.Constants;
@@ -19,7 +19,7 @@ public class Elevator extends SubsystemBase {
   private boolean isBreakMode = false;
 
   private final Configuration talonConfiguration = new Configuration();
-  private final LazyTalonSRX elevatorMotorMaster, elevatorMotorSlave;
+  private final WPI_TalonSRX elevatorMotorMaster, elevatorMotorSlave;
 
   public synchronized static Elevator getInstance() {
     if(instance == null) {
