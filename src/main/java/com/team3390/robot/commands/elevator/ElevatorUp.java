0da66072499d0b162d805441034cc319921533d9
Doruk Karakoç <karakocdoruk4@gmail.com>
2025-01-30 19:48:04 +0300

    elevator

--- src/main/java/com/team3390/robot/commands/elevator/ElevatorUp.java
+++ src/main/java/com/team3390/robot/commands/elevator/ElevatorUp.java
@@ -39,11 +39,6 @@ public class ElevatorUp extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(elevator.atBorder()){
-      return true;
-    }
-    else{
-      return false;
-    }
+    return elevator.atBorder();
   }
 }
